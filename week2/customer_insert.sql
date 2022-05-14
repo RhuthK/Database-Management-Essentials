4709853266fc810326832a9abf4d50af944d1c4a
RhuthK <53269608+RhuthK@users.noreply.github.com>
2022-05-14 20:08:42 +0530

    Create customer_insert.sql

---
+++ week2/customer_insert.sql
@@ -0,0 +1,5 @@
+insert into Customer values ('C100', 'Football','Box 352200', True, 'Mary Manager', 857100, 'Boulder','CO', 80309);
+insert into Customer values ('C101', "Men's Basketball" ,'Box 352400', True, 'Sally Supervisor', 5431700, 'Boulder','CO', 80309);
+insert into Customer values ('C103', 'Basketball','Box 352020', True, 'Bill Baseball', 5431234, 'Boulder','CO', 80309);
+insert into Customer values ('C104', "Women's Softball",'Box 351200', True, 'Sue Softball', 5434321, 'Boulder','CO', 80309);
+insert into Customer values ('C105', 'High School Football','123 AnyStreet', False, 'Coach Bob', 4441234, 'Louisville','CO', 80027);
