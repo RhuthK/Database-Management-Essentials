5e2df85c2288a2af1d151a335291d8160765d0b2
RhuthK <53269608+RhuthK@users.noreply.github.com>
2022-05-14 11:35:25 +0530

    Update employee.sql

--- week2/employee.sql
+++ week2/employee.sql
@@ -1,8 +1,8 @@
 create table Employee(
-	  empNo		    char(10)		  not null, 
-    empName		  varchar(20)		not null, 
-    department	varchar(20)		not null, 
-    email		    varchar(10)		not null, 
+    empNo		    char(10)		not null, 
+    empName		    varchar(20)		not null, 
+    department	            varchar(20)		not null, 
+    email	 	    varchar(10)		not null, 
     phone		    varchar(20)		not null, 
     
     constraint EmployeePK primary key (empNo)
