d43e29ef8db371d64ff31a2242a282d66745d74f
RhuthK <53269608+RhuthK@users.noreply.github.com>
2022-05-14 20:06:49 +0530

    Update customer.sql

--- week2/customer.sql
+++ week2/customer.sql
@@ -1,13 +1,15 @@
+create database universityDB; 
+use universityDB; 
 create table Customer(
 	custNo				char(8)			not null, 
-    	custName			varchar(8)		not null, 
-    	address				varchar(8)		not null, 
+    	custName			varchar(20)		not null, 
+    	address				varchar(20)		not null, 
     	internal 			char(1)			not null, 
     	contact				varchar(20)		not null, 
     	phone				varchar(20)		not null, 
     	city				varchar(20)		not null, 
     	state				char(2)			not null,
-	zip 				varchar(10)		not null, 
+		zip 				varchar(10)		not null, 
     
     	constraint CustomerPK primary key (custNo)
 );
