edafaf9821b27fc5b04dac512cb4acf7dcba20ee
RhuthK <53269608+RhuthK@users.noreply.github.com>
2022-05-14 11:46:29 +0530

    Create eventrequest.sql

---
+++ week2/eventrequest.sql
@@ -0,0 +1,18 @@
+create table EventRequest(
+	eventNo		char(8)		not null, 
+    dateHeld	date		not null, 
+    dateReq		date		not null, 
+    custNo		char(8)		not null, 
+    facNo		char(8)		not null, 
+    dateAuth	date,
+    status		char(20)	not null, 
+    estCost		decimal(10,2)		not null, 
+    estAudience	integer		not null, 
+    budNo		char(20),
+    
+    constraint	EventRequestPK	primary key(eventNo), 
+    constraint	CustomerFK		foreign key(custNo) references Customer(custNo), 
+    constraint 	FacilityFk		foreign key(facNo) references Facility(facNo), 
+    check (status in ('Pending', 'Approved', 'Denied'))
+    
+);
