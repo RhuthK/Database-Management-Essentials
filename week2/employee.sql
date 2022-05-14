e3a4204e563cfef2ddf5771f14c508f0cb794f80
RhuthK <53269608+RhuthK@users.noreply.github.com>
2022-05-14 11:30:36 +0530

    Create employee.sql

---
+++ week2/employee.sql
@@ -0,0 +1,10 @@
+create table Employee(
+	  empNo		    char(10)		  not null, 
+    empName		  varchar(20)		not null, 
+    department	varchar(20)		not null, 
+    email		    varchar(10)		not null, 
+    phone		    varchar(20)		not null, 
+    
+    constraint EmployeePK primary key (empNo)
+
+);
