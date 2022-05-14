5b716dde236dd6548ea1620cff5e0cc9b7a4aae3
RhuthK <53269608+RhuthK@users.noreply.github.com>
2022-05-14 11:56:57 +0530

    Create eventplan.sql

---
+++ week2/eventplan.sql
@@ -0,0 +1,13 @@
+create table EventPlan(
+	planNo		char(8)		not null, 
+    eventNo		char(8)		not null, 
+    workDate	date		not null, 
+    notes		varchar(80),
+    activity	varchar(20)	not null, 
+    empNo		char(10)	not null ,
+    
+    constraint  EventPlanPK	primary key(planNo), 
+    constraint 	EventRequestFk	foreign key(eventNo) references EventRequest(eventNo), 
+    constraint  EmployeeFK		foreign key(empNo)	references Employee(empNo)
+    
+);
