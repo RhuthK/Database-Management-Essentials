361db02c25b4c816c21c315c9627aa721d646e3e
RhuthK <53269608+RhuthK@users.noreply.github.com>
2022-05-15 10:26:40 +0530

    Create employee_insert.sql

---
+++ week2/employee_insert.sql
@@ -0,0 +1,4 @@
+insert into employee values("E100", "Chuck Coordinator","Administration",	"chuck@colorado.edu", 	3-1111);
+insert into employee values("E101", "Mary Manager","Football",	"mary@colorado.edu", 	5-1111);
+insert into employee values("E102", "Sally Supervisor","Planning ",	"sally@colorado.edu", 	3-2222);
+insert into employee values("E103", "Alan Administrator","Administration",	"alan@colorado.edu", 	3-3333);
