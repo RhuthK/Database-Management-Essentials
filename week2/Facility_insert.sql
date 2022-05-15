fc6d945432d70136c4ec71465c5ccebcda9871d3
RhuthK <53269608+RhuthK@users.noreply.github.com>
2022-05-15 10:29:07 +0530

    Create Facility_insert.sql

---
+++ week2/Facility_insert.sql
@@ -0,0 +1,5 @@
+
+insert into Facility values("F100", 'Football stadium');
+insert into Facility values("F101", 'Basketball arena');
+insert into Facility values("F102", 'Baseball field');
+insert into Facility values("F103", 'Recreation room');
