5da364f7fc3b7dcff6b3d0a5e33cf1d4b12ad0ea
RhuthK <53269608+RhuthK@users.noreply.github.com>
2022-05-15 10:36:53 +0530

    Create resourceTbl_insert.sql

---
+++ week2/resourceTbl_insert.sql
@@ -0,0 +1,6 @@
+insert into ResourceTbl values('R100', "attendant", "10.00");
+insert into ResourceTbl values('R101', "police", "15.00");
+insert into ResourceTbl values('R102', "usher", "10.00");
+insert into ResourceTbl values('R103', "nurse", "20.00");
+insert into ResourceTbl values('R104', "janitor", "15.00");
+insert into ResourceTbl values('R105', "food service", "10.00");
