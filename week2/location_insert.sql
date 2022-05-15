1a9c3d7449d3b97640ca3064a570796cb4dc161c
RhuthK <53269608+RhuthK@users.noreply.github.com>
2022-05-15 10:32:50 +0530

    Create location_insert.sql

---
+++ week2/location_insert.sql
@@ -0,0 +1,7 @@
+insert into Location values("L100", "F100", "Locker room");
+insert into Location values("L101", "F100", "Plaza");
+insert into Location values("L102", "F100", "Vehicle room");
+insert into Location values("L103", "F101", "Locker room");
+insert into Location values("L104", "F100", "Ticket Booth");
+insert into Location values("L105", "F101", "Gate");
+insert into Location values("L106", "F100", "Pedestrian gate");
