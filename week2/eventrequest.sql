134338f9b910d23c2a33ade61275bb9f30761680
RhuthK <53269608+RhuthK@users.noreply.github.com>
2022-05-16 10:41:02 +0530

    Update eventrequest.sql

--- week2/eventrequest.sql
+++ week2/eventrequest.sql
@@ -1,11 +1,11 @@
 create table EventRequest(
-	eventNo		char(8)		not null, 
+	eventNo		varchar(8)		not null, 
     dateHeld	date		not null, 
     dateReq		date		not null, 
+    facNo		char(11)		not null, 
     custNo		char(8)		not null, 
-    facNo		char(8)		not null, 
     dateAuth	date,
-    status		char(20)	not null, 
+    status		varchar(20)	not null, 
     estCost		decimal(10,2)   not null, 
     estAudience	integer		not null, 
     budNo		char(20),
@@ -13,7 +13,6 @@ create table EventRequest(
     constraint	EventRequestPK	primary key(eventNo), 
     constraint	CustomerFK		foreign key(custNo) references Customer(custNo), 
     constraint 	FacilityFKK		foreign key(facNo) references Facility(facNo), 
-    check (status in ('Pending', 'Approved', 'Denied'))
-    
-	
+    check (status in ('Pending', 'Approved', 'Denied')),
+    check(estAudience > 0)
 );
