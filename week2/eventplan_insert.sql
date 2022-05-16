6a5ff72c535d2513d65ac7d7fb70310a3a0718f0
RhuthK <53269608+RhuthK@users.noreply.github.com>
2022-05-16 14:49:13 +0530

    Create eventplan_insert.sql

---
+++ week2/eventplan_insert.sql
@@ -0,0 +1,13 @@
+
+insert into EventPlan values('P100', 'E100', '2018-10-25', 'Standard operation','opration', 'E102');
+insert into EventPlan values('P101', 'E104', '2018-12-03', 'Standard operation','opration', 'E102');
+insert into EventPlan values('P102', 'E105', '2018-12-05', 'Standard operation','opration', 'E102');
+insert into EventPlan values('P103', 'E106', '2018-12-12', 'Standard operation','opration', 'E102');
+insert into EventPlan values('P104', 'E101', '2018-10-26', 'Standard operation','opration', 'E102');
+insert into EventPlan values('P105', 'E100', '2018-10-25', 'Standard operation','opration', 'E102');
+insert into EventPlan values('P199', 'E102', '2018-12-10', 'Standard operation','opration', 'E102');
+insert into EventPlan values('P299', 'E101', '2018-10-25', 'Standard operation','opration', 'E102');
+insert into EventPlan values('P349', 'E106', '2018-10-25', 'Standard operation','opration', 'E102');
+insert into EventPlan values('P85', 'E100', '2018-10-25', 'Standard operation','opration', 'E102');
+insert into EventPlan values('P95', 'E101', '2018-10-25', 'Standard operation','opration', 'E102');
+
