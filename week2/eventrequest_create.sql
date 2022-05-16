74ad0738ac8123a15ea2a3ebdef26d49825b4331
RhuthK <53269608+RhuthK@users.noreply.github.com>
2022-05-16 10:41:33 +0530

    Create eventrequest_create.sql

---
+++ week2/eventrequest_create.sql
@@ -0,0 +1,9 @@
+
+insert into EventRequest values('E100', '2013-10-25', '2013-06-06', 'F100', 'C100', '2013-06-08', 'Approved', 5000.00, 80000, 'B1000'); 
+insert into EventRequest values("E101", '2013-10-26', '2013-07-28', 'F100', "C100",  null ,		  "Pending",  5000.00, 80000, 'B1000'); 
+insert into EventRequest values("E102", '2013-09-14', '2013-07-28', 'F100', "C100", '2013-07-31', "Approved", 5000.00, 80000, 'B1000'); 
+insert into EventRequest values("E103", '2013-09-21', '2013-07-28', 'F100', "C100", '2013-08-01', "Approved", 5000.00, 80000, 'B1000'); 
+insert into EventRequest values("E104", '2013-12-03', '2013-07-28', 'F100', "C101", '2013-07-31', "Approved", 2000.00, 120000, 'B1000'); 
+insert into EventRequest values("E105", '2013-12-05', '2013-07-28', 'F101', "C101", '2013-08-01', "Approved", 2000.00, 100000, 'B1000'); 
+insert into EventRequest values("E106", '2013-12-12', '2013-07-28', 'F101', "C101", '2013-07-31', "Approved", 2000.00, 100000, 'B1000'); 
+insert into EventRequest values("E107", '2013-11-23', '2013-07-28', 'F100', "C105", '2013-07-31', "Denied"  , 10000.00, 5000, null); 
