d50275fdbdbaecd8c4c6ac03a740dc8f4b9d39f8
RhuthK <53269608+RhuthK@users.noreply.github.com>
2022-05-16 15:21:05 +0530

    Update eventplan.sql

--- week2/eventplan.sql
+++ week2/eventplan.sql
@@ -1,10 +1,11 @@
+drop table EventPlan; 
 create table EventPlan(
 	planNo		char(8)		not null, 
     eventNo		char(8)		not null, 
     workDate	date		not null, 
     notes		varchar(80),
     activity	varchar(20)	not null, 
-    empNo		char(10)	not null ,
+    empNo		char(10),
     
     constraint  EventPlanPK	primary key(planNo), 
     constraint 	EventRequestFk	foreign key(eventNo) references EventRequest(eventNo), 
