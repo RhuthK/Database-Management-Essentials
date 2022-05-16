12e68f58a6052a4208cca1985e6287d0b982ca22
RhuthK <53269608+RhuthK@users.noreply.github.com>
2022-05-16 15:21:36 +0530

    Update eventplan_insert.sql

--- week2/eventplan_insert.sql
+++ week2/eventplan_insert.sql
@@ -1,13 +1,12 @@
 
-insert into EventPlan values('P100', 'E100', '2018-10-25', 'Standard operation','opration', 'E102');
-insert into EventPlan values('P101', 'E104', '2018-12-03', 'Standard operation','opration', 'E102');
-insert into EventPlan values('P102', 'E105', '2018-12-05', 'Standard operation','opration', 'E102');
-insert into EventPlan values('P103', 'E106', '2018-12-12', 'Standard operation','opration', 'E102');
-insert into EventPlan values('P104', 'E101', '2018-10-26', 'Standard operation','opration', 'E102');
-insert into EventPlan values('P105', 'E100', '2018-10-25', 'Standard operation','opration', 'E102');
-insert into EventPlan values('P199', 'E102', '2018-12-10', 'Standard operation','opration', 'E102');
-insert into EventPlan values('P299', 'E101', '2018-10-25', 'Standard operation','opration', 'E102');
-insert into EventPlan values('P349', 'E106', '2018-10-25', 'Standard operation','opration', 'E102');
-insert into EventPlan values('P85', 'E100', '2018-10-25', 'Standard operation','opration', 'E102');
-insert into EventPlan values('P95', 'E101', '2018-10-25', 'Standard operation','opration', 'E102');
-
+insert into EventPlan values('P100', 'E100', '2018-10-25', 'Standard operation','Operation', 'E102');
+insert into EventPlan values('P101', 'E104', '2018-12-03', 'Watch for gate crashers','Operation', 'E100');
+insert into EventPlan values('P102', 'E105', '2018-12-05', 'Standard operation','Operation', 'E102');
+insert into EventPlan values('P103', 'E106', '2018-12-12', ' Watch for seat switching ','Opration', null);
+insert into EventPlan values('P104', 'E101', '2018-10-26', 'Standard cleanup','Cleanup', 'E101');
+insert into EventPlan values('P105', 'E100', '2018-10-25', 'Light cleanup','Cleanup', 'E101');
+insert into EventPlan values('P199', 'E102', '2018-12-10', 'Standard operation ' ,'Operation', 'E101');
+insert into EventPlan values('P299', 'E101', '2018-10-26', null,'Opration', 'E101');
+insert into EventPlan values('P349', 'E106', '2018-12-12', null,'Cleanup', 'E101');
+insert into EventPlan values('P85', 'E100', '2018-10-25', 'Standard operation','Setup', 'E102');
+insert into EventPlan values('P95', 'E101', '2018-10-26', 'Extra Security','Setup', 'E102');
