0d75d23454d0274dc53c97a5b051c3952b1bb9cd
RhuthK <53269608+RhuthK@users.noreply.github.com>
2022-05-17 10:11:52 +0530

    Create problem1.sql

---
+++ week3/problem1.sql
@@ -0,0 +1,4 @@
+-- 1.	List the customer number, the name, the phone number, and the city of customers -- 
+use universityDB; 
+-- select * from Customer;  
+select custNo, custName, phone, city from Customer; 
