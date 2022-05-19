a19b1156b47dcaec97966ab6cb671d1762435e16
RhuthK <53269608+RhuthK@users.noreply.github.com>
2022-05-19 17:57:44 +0530

    Update m4-problem5.sql

--- week3/m4-problem5.sql
+++ week3/m4-problem5.sql
@@ -1,3 +1,5 @@
+--5.	List the event number, event date (DateHeld), customer number and 
+--customer name of events placed in January 2018 by customers from Boulder.
 select eventNo, dateheld, Customer.custno, Customer.custName 
 from EventRequest inner join Customer
 	on  EventRequest.custno = Customer.custno 
