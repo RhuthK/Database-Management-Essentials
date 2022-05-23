b24bf44cdbdc54f62f28df0bd043f56b5d5f7c07
RhuthK <53269608+RhuthK@users.noreply.github.com>
2022-05-23 08:20:43 +0530

    Create m5-problem4.sql

---
+++ week3/m5-problem4.sql
@@ -0,0 +1,4 @@
+-- 4.	Insert yourself as a new row in the Customer table.
+select * from customer; 
+insert into customer values ('C106', 'Swimming', '126 Hyland', 1,'Anna D', 212112,'Des moines', 'IA',50013 );
+select * from customer; 
