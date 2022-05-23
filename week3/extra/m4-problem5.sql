d80eb80d624a1fd7d6dba62dc1d5401f29658501
RhuthK <53269608+RhuthK@users.noreply.github.com>
2022-05-23 17:49:32 +0530

    Update m4-problem5.sql

--- week3/extra/m4-problem5.sql
+++ week3/extra/m4-problem5.sql
@@ -1,4 +1,4 @@
 -- 5.	Insert yourself as a new row in the Customer table.
-select * from Customer; 
+-- select * from Customer; 
 insert into Customer values ('C0314327', 'Kat','Dahlia', '101 Box St.', 'Des Moines', 'IA', '50000-13',230.12 );
-select * from Customer; 
+-- select * from Customer; 
