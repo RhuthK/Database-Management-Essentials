f598c388c5e5f15eab0b5ed85111ae2f8980e127
RhuthK <53269608+RhuthK@users.noreply.github.com>
2022-05-23 21:17:13 +0530

    Create m5-problem7.sql

---
+++ week3/extra/m5-problem7.sql
@@ -0,0 +1,8 @@
+-- 7.	Increase the price by 10 percent of products containing the words Ink Jet.
+-- In MySQL, you need to place the UPDATE statement between two SET statements.
+SET SQL_SAFE_UPDATES = 0;
+UPDATE Product 
+set ProdPrice = ProdPrice * 1.1
+where ProdName like '%Ink Jet%';
+
+
