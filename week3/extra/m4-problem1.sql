99463743946600a77f4797ecea6c1329e94a893f
RhuthK <53269608+RhuthK@users.noreply.github.com>
2022-05-23 15:15:28 +0530

    Create m4-problem1.sql

---
+++ week3/extra/m4-problem1.sql
@@ -0,0 +1,15 @@
+-- 1.	List the order number, order date, customer number, customer name 
+-- (first and last), employee number, and employee name (first and last) of January 2017 orders 
+-- placed by Colorado customers.
+
+use orderentrydb; 
+show tables; 
+
+select OrderTbl.ordNo, OrderTbl.ordDate, OrderTbl.OrdState, Customer.custNo, 
+ Customer.custFirstName, Customer.custLastname, Employee.EmpNo,  Employee.EmpFirstName, 
+ Employee.EmpLastName
+from Customer, OrderTbl, Employee
+where Customer.CustNo = OrderTbl.CustNo 
+and OrderTbl.EmpNo = Employee.EmpNo 
+and OrderTbl.OrdState = 'CO' 
+and date_format(OrderTbl.OrdDate,'%Y') = '2017' ; 
