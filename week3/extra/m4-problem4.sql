354655b0e65faf167ccfd8289cb65525f8b64ad0
RhuthK <53269608+RhuthK@users.noreply.github.com>
2022-05-23 17:44:39 +0530

    Create m4-problem4.sql

---
+++ week3/extra/m4-problem4.sql
@@ -0,0 +1,11 @@
+-- 4.	List the order number, order date, customer name (first and last), and total amount for 
+-- orders placed on January 23, 2017. The total amount of an order is the sum of the quantity times 
+-- the product price of each product on the order.
+
+select OrderTbl.OrdNo, OrderTbl.OrdDate, Customer.CustFirstName, Customer.CustLastName,
+ sum(Product.ProdPrice * OrdLine.Qty) as total_amount_orders
+ from OrderTbl, Customer, OrdLine, Product
+ where OrderTbl.OrdNo = OrdLine.OrdNo
+ and OrdLine.ProdNo = Product.ProdNo
+ and OrderTbl.CustNo = Customer.CustNo
+ and OrderTbl.OrdDate = '2017-01-23'; 
