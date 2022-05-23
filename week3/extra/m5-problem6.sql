6b2cd81e900574634262c167547b535433f73173
RhuthK <53269608+RhuthK@users.noreply.github.com>
2022-05-23 17:57:33 +0530

    Create m5-problem6.sql

---
+++ week3/extra/m5-problem6.sql
@@ -0,0 +1,3 @@
+-- 6.	Insert an imaginary friend as a new row in the Employee table.
+-- select * from Employee; 
+insert into Employee values('E122594', 'Bob','Chuck','(303)-180-1264','BChuck@bigco.com',null,0.030);
