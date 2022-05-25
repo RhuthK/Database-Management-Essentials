9e2661a6f8cdb98021b85ca63c1a62b4bccc64e0
RhuthK <53269608+RhuthK@users.noreply.github.com>
2022-05-25 21:47:31 +0530

    Create m5-problem6.sql

---
+++ week3/graded/m5-problem6.sql
@@ -0,0 +1,4 @@
+-- 6. Insert a new row in the Location table related to the Facility row in 
+-- modification problem 1. The new row should have “Door” for the location name.
+select * from Location ; 
+insert into Location values ('L107', 'F104', 'Door'); 
