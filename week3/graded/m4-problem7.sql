fc73b3d5aeb92e9c43eb4e9955f82511764f4141
RhuthK <53269608+RhuthK@users.noreply.github.com>
2022-05-26 20:21:51 +0530

    Create m4-problem7.sql

---
+++ week3/graded/m4-problem7.sql
@@ -0,0 +1,3 @@
+-- 7.	Insert a new row in the Location table related to the Facility row in modification problem 1. The new row should have “Locker Room” for the location name.
+
+insert into location values('L08','F104', 'Locker Room'); 
